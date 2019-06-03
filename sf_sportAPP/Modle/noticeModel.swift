d32080926df7c8af5db2a66a96845ea773fed31e
Ray <ray@all.pmlis.wlan.dhcp.208.ippool.cyut.edu.tw>
2019-06-03 18:56:30 +0800

    add model

---
+++ sf_sportAPP/Modle/noticeModel.swift
@@ -0,0 +1,9 @@
+//
+//  noticeModel.swift
+//  sf_sportAPP
+//
+//  Created by Ray   on 2019/6/3.
+//  Copyright © 2019 Ray  . All rights reserved.
+//
+
+import Foundation
---
+++ sf_sportAPP/Views/NoticeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NoticeTableViewCell.swift
+//  sf_sportAPP
+//
+//  Created by Ray   on 2019/6/3.
+//  Copyright © 2019 Ray  . All rights reserved.
+//
+
+import UIKit
+
+class NoticeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
