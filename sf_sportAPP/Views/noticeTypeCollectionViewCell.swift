af1bca3747ee2b478d7a4e673d05b889369afec7
Ray <ray@zhongzhihangde-MacBook-Pro.local>
2019-06-08 02:10:43 +0800

    1.公告選擇類型更改介面

---
+++ sf_sportAPP/Views/noticeTypeCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  noticeTypeCollectionViewCell.swift
+//  sf_sportAPP
+//
+//  Created by Ray   on 2019/6/7.
+//  Copyright © 2019 Ray  . All rights reserved.
+//
+
+import UIKit
+
+class noticeTypeCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
